09ab59ba1334493b8b9d735228a151d823ba04c7
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-01-17 08:19:29 -0800

    WIP

--- src/scanner.py
+++ src/scanner.py
@@ -5,22 +5,35 @@ SCANNER
 """
 
 import pyparsing as pp
+from typing import Any
 
 
 def parse_lines(lines: list[str], verbose: bool = False) -> None:
+    REgrammar: Any = define_RE_grammar()
+
     for line in lines:
-        tokens: list[str] = parse_line(line, verbose)
+        tokens: list[str] = parse_line(line, REgrammar, verbose)
 
         pass
 
 
-def parse_line(line: str, verbose: bool = False) -> list[str]:
-    literal: pp.QuotedString = pp.QuotedString('"', unquote_results=False)
-
-    REgrammar = literal
-    tokens: list[str] = REgrammar.parseString(line)
+def parse_line(line: str, grammar, verbose: bool = False) -> list[str]:
+    tokens: list[str] = grammar.parseString(line)
 
     if verbose:
         print(tokens)
 
     return tokens
+
+
+def define_RE_grammar() -> Any:
+    literal: pp.QuotedString = pp.QuotedString('"', unquote_results=False)
+
+    REgrammar = literal
+    REgrammar.ignore(pp.pythonStyleComment)
+
+    return REgrammar
+
+
+# TODO - Abstract specification of grammar from parsing using it
+# TODO - Use pp.ParseFile
