a46a51585aeb0e95a79f5636e4a0794495643d64
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-01-27 06:35:12 -0800

    WIP

--- src/modes.py
+++ src/modes.py
@@ -44,32 +44,66 @@ mode_groups_def: ParserElement = (
 beg_modes_def: ParserElement = Suppress(modes_word) + Suppress("(")
 end_modes_def: ParserElement = Suppress(")")
 
+# TODO
+# @beg_modes_def.set_parse_action
+# def beg_modes_act(toks: ParseResults) -> str:
+#     translation: str = "(?:"
+#     comment: str = f"Begin inline mode modifiers:"
 
-@beg_modes_def.set_parse_action
-def beg_modes_act(toks: ParseResults) -> str:
-    translation: str = "(?:"
-    comment: str = f"Begin inline mode modifiers:"
-
-    return modal_act(
-        toks,
-        translation,
-        comment,
-        tab_inc=1,
-        tok_list=True,
-    )
+#     return modal_act(
+#         toks,
+#         translation,
+#         comment,
+#         tab_inc=1,
+#         tok_list=True,
+#     )
 
 
-@end_modes_def.set_parse_action
-def end_modes_act(toks: ParseResults) -> str:
-    return modal_act(
-        toks, ")", f"End of inline mode modifiers", tab_inc=-1, tok_list=True
-    )
+# TODO
+# @end_modes_def.set_parse_action
+# def end_modes_act(toks: ParseResults) -> str:
+#     return modal_act(
+#         toks, ")", f"End of inline mode modifiers", tab_inc=-1, tok_list=True
+#     )
+
+
+def translate_modes(toks) -> str:
+    """Translate a combined list of on/off modes to a regex string."""
+
+    translation: str = ""
+
+    if isinstance(toks[0], str):  # Either all on or all off
+        translation = "(?" + translate_mode_list(toks) + ")"
+
+    else:  # Mixed on/off
+        translation = (
+            "(?" + translate_mode_list(toks[0]) + translate_mode_list(toks[1]) + ")"
+        )
+
+    return translation
+
+
+def translate_mode_list(toks) -> str:
+    """Translate a list of either on or off modes to a portion of a regex string."""
+
+    translation: str = ""
+    translation += "" if toks[0] == "on" else "-"
+    for tok in toks[1:]:
+        # token: str = unpack_token(toks, grouped=True)
+        translation += translate_word(tok)
+
+    return translation
 
 
 # TODO
 @mode_groups_def.set_parse_action
-def mode_groups_act(toks: ParseResults) -> str:
-    return modal_act(toks, "TODO", f"TODO")
+def mode_groups_act(toks: list) -> str:
+    translation: str = "TODO"
+    comment: str = "TODO"
+
+    # translation: str = translate_modes(toks)
+    # comment: str = f"Inline mode modifiers"
+    return modal_act(toks, translation, comment)
 
 
 modes_pattern_def: ParserElement = beg_modes_def + mode_groups_def + end_modes_def
